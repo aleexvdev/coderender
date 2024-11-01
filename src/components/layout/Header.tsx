d98b7835b112308474fac13520f3751cfbadc9be
aleexvdev <valverdealex25@gmail.com>
2024-11-01 18:02:54 -0500

    feat: add link project

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -17,9 +17,6 @@ const containerVariants = {
 };
 
 export const Header = () => {
-
-  
-
   return (
     <motion.header
       className="w-full px-2 md:px-4 lg:px-8"
@@ -29,20 +26,25 @@ export const Header = () => {
     >
       <div className="w-full">
         <div className="w-full h-16 flex items-center justify-between">
-          <motion.div
-            className="text-2xl md:text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-700 via-blue-600 to-blue-800 select-none"
-            whileHover={{ scale: 1.02 }}
-          >
-            Cod
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 via-blue-700 to-blue-800">
-              eRen
-            </span>
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 via-blue-700 to-blue-800">
-              der
-            </span>
-          </motion.div>
+          <a href="https://github.com/aleexvdev/coderender" target="_blank">
+            <motion.div
+              className="text-2xl md:text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-700 via-blue-600 to-blue-800 select-none"
+              whileHover={{ scale: 1.02 }}
+            >
+              Cod
+              <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 via-blue-700 to-blue-800">
+                eRen
+              </span>
+              <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 via-blue-700 to-blue-800">
+                der
+              </span>
+            </motion.div>
+          </a>
           <div className="flex items-center space-x-2 md:space-x-4">
-            <Button className="px-4 py-2 rounded-md focus:outline-none hover:bg-[#2e2e2e] hidden md:flex" variant="outline">
+            <Button
+              className="px-4 py-2 rounded-md focus:outline-none hover:bg-[#2e2e2e] hidden md:flex"
+              variant="outline"
+            >
               <Keyboard className="mr-1 h-5 w-5" /> Keywords
             </Button>
             <ExportOption />
